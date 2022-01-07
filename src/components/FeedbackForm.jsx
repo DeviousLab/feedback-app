648eec6dc8e06429ae723f997febad5cd72592b5
DeviousLab <deviouslab@gmail.com>
2022-01-07 21:28:40 +0400

    Added form validation before submitting review
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,20 @@ import Button from './Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if(text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Please enter at least 10 characters');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
     setText(e.target.value);
   }
 
@@ -16,8 +28,9 @@ function FeedbackForm() {
         <h2>How would you rate our services?</h2>
         <div className="input-group">
           <input onChange={handleTextChange} type="text" name="review" id="review" placeholder="Type your review here" value={text} />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <p className="message">{message}</p>}
       </form>
     </Card>
   )
