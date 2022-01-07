eac9317a22ae11b8b4ccd164edd0f0a47bb035dc
DeviousLab <deviouslab@gmail.com>
2022-01-07 22:21:31 +0400

    Added feedback and about pages to separate routes
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

---
+++ src/components/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import Card from "../Card"
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About Us</h1>
+        <p>This is a project built with React to leave feedback</p>
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
