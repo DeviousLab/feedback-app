c0b774ded40473a2b45c62bedaa945d5c0fb4aa3
DeviousLab <deviouslab@gmail.com>
2022-01-09 23:49:54 +0400

    Transferred all functions and reviews to context API file
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
-import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./components/pages/AboutPage";
@@ -12,18 +9,6 @@ import AboutIconLink from "./components/AboutIconLink";
 import { FeedbackProvider } from "./context/FeedbackContext";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete this feedback?")) {
-      setFeedback(feedback.filter((feedback) => feedback.id !== id));
-    }
-  }
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  }
 
   return (
     <FeedbackProvider>
@@ -33,9 +18,9 @@ function App() {
           <Routes>
             <Route exact path="/" element={
               <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                <FeedbackForm />
+                <FeedbackStats />
+                <FeedbackList />
               </>
             }>
             </Route>
